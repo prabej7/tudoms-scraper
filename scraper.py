0b47c4665adbd044605351c983797bf1692e6e48
Mahammad Prabej Hussain <contact.prabej@gmail.com>
2026-09-23 19:13:03 +0545

    feat: add 6th sem keyword and cron config

--- scraper.py
+++ scraper.py
@@ -50,6 +50,7 @@ def scrape_result():
         if (
             "result published" in text.lower()
             and "bim" in text.lower()
+            and "6th"
         ):
             href = urljoin(URL, link["href"])
 
